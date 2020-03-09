75f80311f55a559af018f258e5dbce1e8cc5e674
Senghong Hour <hoursenghong@gmail.com>
2020-03-09 09:31:12 +0700

    added project files and load btns

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var loadApron = document.getElementById("load-apron");
+var slotApron = document.getElementById("slot-apron");
+
+loadApron.addEventListener("click", function() {
+    slotApron.src = "data/project/apron.png";
+});
+
+var loadBonjour = document.getElementById("load-bonjour");
+var slotBonjour = document.getElementById("slot-bonjour");
+
+loadBonjour.addEventListener("click", function() {
+    slotBonjour.src = "data/project/bonjour.png";
+});
+
+var loadCambodiana = document.getElementById("load-cambodiana");
+var slotCambodiana = document.getElementById("slot-cambodiana");
+
+loadCambodiana.addEventListener("click", function() {
+    slotCambodiana.src = "data/project/cambodiana.png";
+});
+
+var loadVilla = document.getElementById("load-villa");
+var slotVilla = document.getElementById("slot-villa");
+
+loadVilla.addEventListener("click", function() {
+    slotVilla.src = "data/project/villa.png";
+});
+
+var loadLateur = document.getElementById("load-lateur");
+var slotLateur = document.getElementById("slot-lateur");
+
+loadLateur.addEventListener("click", function() {
+    slotLateur.src = "data/project/lateur.png";
+});
+
+var loadArtisans = document.getElementById("load-artisans");
+var slotArtisans = document.getElementById("slot-artisans");
+
+loadArtisans.addEventListener("click", function() {
+    slotArtisans.src = "data/project/artisansangkor.png";
+});
